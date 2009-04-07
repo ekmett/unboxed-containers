68d4af8e89babad9da53ca249d2db7c2e62cea7e
ekmett <ekmett@gmail.com>
2009-04-06 20:20:56 -0700

    added a ton of instances
    Ignore-this: fb3fbee87dae3a4acb37a8774cd46efb
    
    darcs-hash:20090407032056-7b3c0-b654d66759ca41a8a4e82fe6671e0b6c360e7510.gz

--- Benchmark.hs
+++ Benchmark.hs
@@ -11,15 +11,23 @@ forceList m = eval m . foldr seq ()
 
 n = 2^18
 
+instance (Random x, Random y) => Random (x,y) where
+    random g = ((a,b),g'') where
+        (a,g') = random g
+        (b,g'') = random g'
+    randomR ((la,lb),(ha,hb)) g = ((a,b),g'') where
+        (a,g') = randomR (la,ha) g
+        (b,g'') = randomR (lb,hb) g'
+
 main = do
     let g  = mkStdGen 42
-        rs :: [Int]
+        rs :: [(Int,Int)]
         rs = take n (randoms g)
     forceList "Generating random numbers" rs
-    testU rs
     testB rs
+    testU rs
     test rs
-    testI rs
+    -- testI rs
 
 test rs = do
     s <- eval "constructing Set         " $ S.fromList rs
@@ -33,6 +41,8 @@ testU rs = do
     s <- eval "constructing USet        " $ U.fromList rs
     forceList "USet.member              " $ map (flip U.member s) rs
 
+{-
 testI rs = do
     s <- eval "constructing IntSet      " $ I.fromList rs
     forceList "IntSet.member            " $ map (flip I.member s) rs
+-}
